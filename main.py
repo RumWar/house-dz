507ec6e2a0d6f65d8d5fd010bddd9156f83acda5
RumWar <danila.yanvarev2004@gmail.com>
2023-09-05 19:28:37 +0300

    end

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from tkinter import *
 
+def clicked():
+    lbl.configure(text="WoW")
 
 window = Tk()
 window.title("W")
-lbl = Label(window, text="W", font=("Arial Bold", 50))
+window.geometry('200x200')
+lbl = Label(window, text="", fg="red")
 lbl.grid(column=0, row=0)
+btn = Button(window, text="W", font=("Arial Bold", 50), bg="black", fg="red", command=clicked)
+btn.grid(column=1, row=1)
 
 
 window.mainloop()
